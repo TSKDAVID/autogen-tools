dc8c8013b2ae466ca35bbeb40ee15ed7aedd9395
davit tskabelia <64692972+TSKDAVID@users.noreply.github.com>
2024-06-09 09:09:58 +0200

    new tool
    pdf maker tool that converts html into pdf

---
+++ pdf_maker.py
@@ -0,0 +1,17 @@
+def pdf_maker():
+
+    path_to_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'  # Update this path
+    config = pdfkit.configuration(wkhtmltopdf=path_to_wkhtmltopdf)
+
+    # Enable local file access
+    options = {
+        'enable-local-file-access': '',
+        'quiet': ''
+    }
+
+    # Convert HTML file to PDF
+    pdfkit.from_file(f'test.html', f'output.pdf', configuration=config, options=options) #change the 'test.html' and 'output.pdf' into names that you need, test html is a file that needs to e converted and
+                                                                                         #and output.pdf is an output file
+
+#in this case this is for windows computer, you would need to install wkhtmltopdf from their website and the file path would still work if you install it in default directory 
+#in case you are using mac os you would tipically change that path to: /usr/local/bin/wkhtmltopdf or you could type which wkhtmltopdf and see where it is and put that path
